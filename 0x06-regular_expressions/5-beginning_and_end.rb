ccd3f81c77be737f64e56bf047a718abb39d5092
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-30 10:12:59 -0800

    A Ruby script accepting one argument and passes it to a regex matching method, must be exactly matching a string that starts with 'h' ends with 'n' and can have any single character in between

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-#
+# A Ruby script that accepts one argument and pass it to a regex matching method
+# The regex must be exactly matching a string that starts with 'h' and ends with 'n' and can have any single character in between
+
+puts ARGV[0].scan(/^h.n$/).join
