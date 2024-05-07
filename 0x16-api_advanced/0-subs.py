9892fefc42461d96e51d77ff94f467b771197b79
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-05-07 16:23:29 -0700

    first commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,12 +12,12 @@ def number_of_subscribers(subreddit):
 
     # A custom User-Agent to avoid Too Many Requests
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by u/Wunderkind94)"
-            }
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by u/Wunderkind94)"
+    }
 
     # A GET requst to Reddit's API
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    res = response.json().get('data')
-    return res.get('subscribers')
+    res = response.json().get("data")
+    return res.get("subscribers")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Python Interpreter.'''
+
+import requests
+
+
+def top_ten(subreddit):
+    '''
+    Function to query the Reddit API and
+    prints the titles of the first 10 hot posts.
+    '''
+    request = requests.get(
+            "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+            headers={"User-Agent": "Custom"},
+            params={"limit": 10},
+    )
+
+    if request.status_code == 200:
+        for get_data in request.json().get("data").get("children"):
+            hot_post = get_data.get("data")
+            title = hot_post.get("title")
+            print(title)
+    else:
+        print(None)
