c5d9af234d612801fd4e7d8731b37724328bd299
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-05-15 07:39:43 -0700

    first commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Fixing the stack to get rid of failed requests
 
 exec {'modifying the logs settings':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart'',
-  path	  => 'usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path	  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
